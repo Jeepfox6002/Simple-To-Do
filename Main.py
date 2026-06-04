88cc50739d1e247c6db07b42c10774865b2db3c2
Jeepfox6002 <loganmtrumm@gmail.com>
2026-06-04 10:13:55 -0500

    Implement JSON data handling and settings menu
    Added JSON file handling and settings window.

--- Main.py
+++ Main.py
@@ -1,22 +1,32 @@
 import tkinter as tk
+import json
+
+needed_data = {
+    "Tasks": ["Example 1", "Example 2"]
+}
 
 def new_file():
-    print("Placeholder")
+    with open("data.json", "w") as f:
+        json.dump(needed_data, f, indent=4)
 
 def open_file():
-    print("placeholder")
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        print(data)
 
 def save_file():
-    print("placeholder")
+    with open("data.json", "w") as file:
+        json.dump(needed_data, file, indent=4, sort_keys=True)
 
-def save_file_as():
-    print("placeholder")
+def settings():
+    root_settings = tk.Toplevel(root)
+    root_settings.geometry("300x200")
+    root_settings.config(bg="#2F2F2F")
+    root_settings.title("Settings")
 
 root = tk.Tk()
-root.geometry("600x400")
-
-text_area = tk.Text(root)
-text_area.pack(expand=True, fill='both')
+root.geometry("580x355")
+root.config(bg="#2F2F2F")
 
 menubar = tk.Menu(root)
 
@@ -24,7 +34,7 @@ file_menu = tk.Menu(menubar, tearoff=0)
 file_menu.add_command(label="New", command=new_file)
 file_menu.add_command(label="Open", command=open_file)
 file_menu.add_command(label="Save", command=save_file)
-file_menu.add_command(label="Save As", command=save_file_as)
+file_menu.add_command(label="Settings", command=settings)
 
 menubar.add_cascade(label="File", menu=file_menu)
 
