aaf7511fc4c7db78ac4e55cb3761c98329d9b6bb
Jeepfox6002 <loganmtrumm@gmail.com>
2026-06-05 22:31:16 -0500

    Refactor Main.py to enhance task management features

--- Main.py
+++ Main.py
@@ -1,19 +1,28 @@
 import tkinter as tk
 import json
 from PIL import Image, ImageTk
+import time
+
+Task_height = 580
+Task_width = 400
+
+Time_until_height = 280
+Time_until_width = 570
+
+Time_left_height = 280
+Time_left_width = 570
+
+Task_panel_height = 150
+
+p = "Placeholder"
 
 needed_data = {
-    "Tasks": ["Example 1", "Example 2"]
+    "Tasks": []
 }
 
 def new_file():
     with open("data.json", "w") as f:
         json.dump(needed_data, f, indent=4)
-
-def open_file():
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-        print(data)
 
 def save_file():
     with open("data.json", "w") as file:
@@ -25,22 +34,33 @@ def settings():
     root_settings.config(bg="#2F2F2F")
     root_settings.title("Settings")
 
-def edit_tasks():
-    print("Placeholder")
+def edit_tasks(event=None):
+    print(p)
 
-Task_height = 580
-Task_width = 400
+def add_tasks(event=None):
+    root__add_task = tk.Toplevel(root)
+    root__add_task.geometry("450x325")
+    root__add_task.config(bg="#2F2F2F")
+    root__add_task.title("Add task")
 
-Time_until_height = 280
-Time_until_width = 570
+    task_name = tk.Entry(root__add_task, bg="#292929")
+    task_name.place(x=10,y=10, width=430)
+    task_name.insert(0,"Type the name of your task here!")
+    placeholder = "Type the name of your task here!"
+    name = task_name.get()
+    def remove_placeholder(event):
+        if name == placeholder:
+            task_name.delete(0,tk.END)
+    task_name.bind("<FocusIn>",remove_placeholder)
 
-Time_left_height = 280
-Time_left_width = 570
+    submit = tk.Button(root__add_task,background="#292929", text="Submit",fg="White")
+    submit.place(x=370,y=285,width=70,height=30)
 
 root = tk.Tk()
 root.geometry("1000x600")
 root.config(bg="#2F2F2F")
 root.title("Simple To-Do")
+root.resizable(False, False)
 
 tasks_canvas = tk.Canvas(root, bg="#292929", width=Task_width, height=Task_height)
 tasks_canvas.propagate(False)
@@ -49,10 +69,15 @@ tasks_canvas.place(x=10, y=10, width=Task_width, height=Task_height)
 task_title = tasks_canvas.create_text(40,20, text="Tasks:", fill="white", font=("Arial", 15, "normal"))
 
 original_edit_image = Image.open("Simple To Do/EditIcon.png")
+original_add_image = Image.open("Simple To Do/AddIcon.png")
+add_image = ImageTk.PhotoImage(original_add_image.resize((30, 30)))
 edit_image = ImageTk.PhotoImage(original_edit_image.resize((30, 30)))
 edit_button = tk.Label(root, image=edit_image, bg="#292929")
-edit_button.place(x=360, y=10)
-edit_button.bind("<Button-1>", edit_tasks())
+edit_button.place(x=360, y=15)
+edit_button.bind("<Button-1>", edit_tasks)
+add_button = tk.Label(root, image=add_image, bg="#292929")
+add_button.place(x=320, y=15)
+add_button.bind("<Button-1>", add_tasks)
 
 time_until_canvas = tk.Canvas(root, bg="#292929", width=Time_until_width, height=Time_until_height)
 time_until_canvas.propagate(False)
@@ -62,17 +87,17 @@ time_left_canvas = tk.Canvas(root, bg="#292929", width=Time_left_width, height=T
 time_left_canvas.propagate(False)
 time_left_canvas.place(x=420, y=310, width=Time_left_width, height=Time_left_height)
 
-menubar = tk.Menu(root)
+#menubar = tk.Menu(root)
 
-file_menu = tk.Menu(menubar, tearoff=0)
-file_menu.add_command(label="New", command=new_file)
-file_menu.add_command(label="Open", command=open_file)
-file_menu.add_command(label="Save", command=save_file)
-file_menu.add_command(label="Settings", command=settings)
+#file_menu = tk.Menu(menubar, tearoff=0)
+#file_menu.add_command(label="New", command=new_file)
+#file_menu.add_command(label="Open", command=open_file)
+#file_menu.add_command(label="Save", command=save_file)
+#file_menu.add_command(label="Settings", command=settings)
 
-menubar.add_cascade(label="File", menu=file_menu)
+#menubar.add_cascade(label="File", menu=file_menu)
 
-root.config(menu=menubar)
+#root.config(menu=menubar)
 
 root.lift()
 root.attributes('-topmost', True)
