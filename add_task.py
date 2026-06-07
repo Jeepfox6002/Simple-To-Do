33a0c8d42b9b9465c2474eb3d36dad03f932b107
Jeepfox6002 <loganmtrumm@gmail.com>
2026-06-07 13:04:54 -0500

    Enhance UI elements in add_task.py

--- add_task.py
+++ add_task.py
@@ -4,10 +4,9 @@ from tkinter import Tk
 
 p = "Placeholder"
 
-def save_file():
-    with open("data.json", "w") as file:
-        json.dump(needed_data, file, indent=4, sort_keys=True)
+save_data = {
 
+}
 
 def submit_task_add(event=None):
     print(p)
@@ -18,7 +17,7 @@ def add_tasks(event=None):
     root_add_task.config(bg="#2F2F2F")
     root_add_task.title("Add task")
 
-    task_name = tk.Entry(root_add_task, bg="#292929")
+    task_name = tk.Entry(root_add_task, bg="#292929", highlightbackground="#1a1a1a", highlightcolor="#1a1a1a", highlightthickness=2, fg="White", bd=0)
     task_name.place(x=10,y=10, width=430,height=40)
     placeholder = "Type the name of your task here!"
     task_name.insert(0,placeholder)
@@ -28,14 +27,14 @@ def add_tasks(event=None):
             task_name.delete(0,tk.END)
     task_name.bind("<FocusIn>",remove_placeholder)
 
-    selection_one = tk.Label(root_add_task,bg="#292929",fg="White",text="When will this task start?")
+    selection_one = tk.Label(root_add_task,bg="#292929",fg="White",text="When will this task start?", highlightbackground="#1a1a1a", highlightthickness=2)
     selection_one.place(x=10, y=60, height=40, width=170)
 
-    at_hour = tk.Entry(root_add_task, bg="#292929")
+    at_hour = tk.Entry(root_add_task, bg="#292929", highlightbackground="#1a1a1a",highlightcolor="#1a1a1a", highlightthickness=2, fg="White", bd=0)
     at_hour.place(x=190,y=60,height=40,width=70)
     at_hour.insert(0, "Hour")
 
-    at_minute = tk.Entry(root_add_task, bg="#292929")
+    at_minute = tk.Entry(root_add_task, bg="#292929", highlightbackground="#1a1a1a", highlightcolor="#1a1a1a", highlightthickness=2, fg="White", bd=0)
     at_minute.place(x=270,y=60,width=70,height=40)
     at_minute.insert(0,"Minute")
 
